ab1564b6238f911f874c9e75265f9096e44bf4de
Alex Samorukov <samm@os2.kiev.ua>
2012-11-16 20:54:33 +0100

    adding files to git

---
+++ jvpn.pl
@@ -0,0 +1,450 @@
+#!/usr/bin/perl
+
+# Script to run ncsvc without JAVA gui and web browser
+
+use strict;
+use warnings;
+use Term::ReadKey;
+use IO::Socket::INET;
+use Fcntl ':mode';
+use Getopt::Long;
+use HTTP::Request::Common;
+use LWP::UserAgent;
+use HTTP::Cookies;
+use File::Copy;
+
+my %Config;
+my $config_file='jvpn.ini';
+my $show_help=0;
+
+GetOptions ("config_file=s" => \$config_file,
+	"help" => \$show_help);
+
+if($show_help) { print_help(); }
+
+# parse configuration
+&parse_config_file ($config_file, \%Config);
+
+my $dhost=$Config{"host"};
+my $dport=$Config{"port"};
+my $username=$Config{"username"};
+my $realm=$Config{"realm"};
+# debug, set to 1 to enable
+my $dnsprotect=$Config{"dnsprotect"};
+my $debug=$Config{"debug"};
+my $verifycert=$Config{"verifycert"};
+
+# checking if we running under root
+
+my $is_setuid = 0;
+if (-e "./ncsvc") {
+	my $mode = (stat("./ncsvc"))[2];
+	$is_setuid = ($mode & S_ISUID) && ((stat("./ncsvc"))[4]== 0);
+	if(!-x "./ncsvc"){
+		print "./ncsvc is not executable, exiting\n"; 
+		exit 1;
+	}
+}
+if( $> != 0 && !$is_setuid) {
+	print "Please, run this script with su/sudo or set suid attribute on 'ncsvc' \n";
+	exit 1;
+}
+
+my $ua = LWP::UserAgent->new;
+# on RHEL6 ssl_opts is not exists
+if(defined &LWP::UserAgent::ssl_opts) {
+    $ua->ssl_opts('verify_hostname' => $verifycert);
+}
+$ua->cookie_jar({});
+push @{ $ua->requests_redirectable }, 'POST';
+
+print "Enter PIN+passsword: ";
+my $password=read_password();
+print "\n";
+
+my $response_body = '';
+
+my $res = $ua->post("https://$dhost:$dport/dana-na/auth/url_default/login.cgi",
+	[ btnSubmit   => 'Sign In',
+	password  => $password,
+	realm => $realm,
+	tz   => '60',
+	username  => $username,
+	]);
+
+$response_body=$res->decoded_content;
+my $dsid="";
+my $dlast="";
+my $dfirst="";
+
+# Looking at the results...
+if ($res->is_success) {
+	print("Transfer went ok\n");
+	# next token request
+	if ($response_body =~ /name="frmNextToken"/) {
+		$response_body =~ m/name="key" value="([^"]+)"/;
+		my $key=$1;
+		print  "The server requires that you enter an additional token ".
+			"code to verify that your credentials are valid.\n".
+			"To continue, wait for the token code to change and ".
+			"then enter the new pin and code.\n";
+		
+		print "Enter PIN+passsword: ";
+		my $password=read_password();
+		print "\n";
+		my $res = $ua->post("https://$dhost:$dport/dana-na/auth/url_default/login.cgi",
+			[ Enter   => 'secidactionEnter',
+			password  => $password,
+			key  => $key,
+			]);
+		$response_body=$res->decoded_content;
+	}
+	# active sessions found
+	if ($response_body =~ /id="DSIDConfirmForm"/) {
+		$response_body =~ m/name="FormDataStr" value="([^"]+)"/;
+		print "Active sessions found, reconnecting...\n";
+		my $res = $ua->post("https://$dhost:$dport/dana-na/auth/url_default/login.cgi",
+			[ btnContinue   => 'Continue the session',
+			FormDataStr  => $1,
+			]);
+		$response_body=$res->decoded_content;
+		
+	}
+	
+	my $cookie=$ua->cookie_jar->as_string;
+	if ( $cookie =~ /DSID=([a-f\d]+)/){
+		$dsid=$1;
+	}
+	if ( $cookie =~ /DSFirstAccess=(\d+)/){
+		$dfirst=$1;
+	}
+	if ( $cookie =~ /DSLastAccess=(\d+)/){
+		$dlast=$1;
+	}
+	if ( $response_body =~ /Invalid username or password/){
+		print "Invalid username or password, exiting \n";
+		exit 1;
+	}
+	
+	# do not print DSID in normal mode for security reasons
+	print $debug?"Got DSID=$dsid, dfirst=$dfirst, dlast=$dlast\n":"Got DSID\n";
+	
+	if ($dsid eq "" || $dfirst eq "" || $dlast eq "") {
+		print "Unable to get data, exiting \n";
+		exit 1;
+	}
+	
+} else {
+	# Error code, type of error, error message
+		print("An error happened: ".$res->status_line."\n");
+	exit 1;
+}
+
+# set int handlers
+$SIG{'INT'}  = \&INT_handler; # CTRL+C
+$SIG{'TERM'} = \&INT_handler; # Kill process
+$SIG{'HUP'} = \&INT_handler; # Terminal closed
+
+# flush after every write
+$| = 1;
+
+# getting MD5 hash of the certificate with openssl s_client
+my $md5hash = <<`SHELL`;
+echo | openssl s_client -connect ${dhost}:${dport} 2>/dev/null| \
+openssl x509 -md5 -noout -fingerprint|\
+awk -F\= '{print \$2}'|tr -d \:
+exit 0
+SHELL
+
+chop($md5hash);
+# changing case
+$md5hash =~ tr/A-Z/a-z/;
+
+if($md5hash eq "") {
+	print "Unable to get md5 hash of certificate, exiting";
+	exit 1;
+}
+
+print "Certificate fingerprint:  [$md5hash]\n";
+
+if (!-e "./ncsvc") {
+	$res = $ua->get ("https://$dhost:$dport/dana-cached/nc/ncLinuxApp.jar",':content_file' => './ncLinuxApp.jar');
+	print "Client not exists, downloading from https://$dhost:$dport/dana-cached/nc/ncLinuxApp.jar\n";
+	if ($res->is_success) {
+		print "Done, extracting\n";
+		system("unzip ncLinuxApp.jar ncsvc && chmod +x ./ncsvc");
+	}
+	else {
+		print "Download failed, exiting\n";
+		exit 1;
+	}
+}
+my $start_t = time;
+system("./ncsvc >/dev/null 2>/dev/null &");
+sleep(3);
+
+my ($socket,$client_socket);
+
+# connecting to ncsvc using TCP
+$socket = new IO::Socket::INET (
+	PeerHost => '127.0.0.1',
+	PeerPort => '4242',
+	Proto => 'tcp',
+	) or die "ERROR in Socket Creation : $!\n";
+
+print "TCP Connection to ncsvc process established.\n";
+
+# sending first packet, got it from tcpdump
+print "Sending handshake #1 packet... ";
+my $data =   "\0\0\0\0\0\0\0\x64\x01\0\0\0\0\0\0\0\0\0\0\0";
+
+if($debug) {hdump($data);}
+print $socket "$data";
+$socket->recv($data,2048);
+# XXX - good idea to chek if it valid
+print " [done]\n";
+if($debug) {hdump($data);}
+
+# second packet from tcpdump
+# it contains logging level in the end:
+# 0 LogLevel 0
+# 10 LogLevel 1
+# 20 LogLevel 2
+# 30 LogLevel 3
+# 40 LogLevel 4
+# 50 LogLevel 5
+# We are enabling full log if debug is enabled
+$data= "\0\0\0\0\0\0\0\x7c\x01\0\0\0\x01\0\0\0\0\0\0\x10\0\0\0\0\0\x0a\0\0".
+        "\0\0\0\x04\0\0\0".($debug?"\x32":"\0");
+print "Sending handshake #2 packet... ";
+if($debug) {hdump($data);}
+print $socket "$data";
+$socket->recv($data,2048);
+# XXX - good idea to chek if it valid
+print " [done]\n";
+if($debug) {hdump($data);}
+# Configuration packet
+# XXX - no idea how it works on non default port
+$data="\0\0\0\0\0\0\0\x66\x01\0\0\0\x01\0\0\0\0\0\0\xcb\0\xcb\0\0".
+	"\0\xc5\0\x01\0\0\0\x1a".
+	$dhost.
+	"\0\0\x02\0\0\0\x78".
+	"DSSignInURL=/; DSID=$dsid; DSFirstAccess=$dfirst; DSLastAccess=$dlast; path=/; secure".
+	"\0\0\x0a\0\0\0\x21".
+	$md5hash.
+	"\0";
+
+print "Sending configuration packet...";
+if($debug) {hdump($data);}
+print $socket "$data";
+$socket->recv($data,2048);
+print " [done]\n";
+
+
+if($debug) {hdump($data);}
+
+# checking reply status
+my @result = unpack('C*',$data);
+my $status = sprintf("%02x",$result[7]);
+# 0x6d seems to be "Connect ok" message
+# exit on any other values
+
+if($status ne "6d") {
+	printf("Status=$status\nAuthentification failed, exiting\n");
+	system("./ncsvc -K");
+	exit(1);
+}
+
+if($> == 0 && $dnsprotect) {
+	system("chattr +i /etc/resolv.conf");
+}
+
+# information query
+$data =  "\0\0\0\0\0\0\0\x6a\x01\0\0\0\x01\0\0\0\0\0\0\0";
+if($debug) {hdump($data);}
+print $socket "$data";
+$socket->recv($data,2048);
+if($debug) {hdump($data);}
+
+print "IP: ".inet_ntoa(pack("N",unpack('x[48]N',$data))).
+	" Gateway: ".inet_ntoa(pack("N",unpack('x[68]N',$data))).
+	"\nDNS1: ".inet_ntoa(pack("N",unpack('x[84]N',$data))).
+	"  DNS2: ".inet_ntoa(pack("N",unpack('x[94]N',$data))).
+	"\nConnected to $dhost, press CTRL+C to exit\n";
+# disabling cursor
+print "\e[?25l";
+while ( 1 ) {
+	#stat query
+	$data="\0\0\0\0\0\0\0\x69\x01\0\0\0\x01\0\0\0\0\0\0\0";
+	print "\r                                                              \r";
+	if($debug) {hdump($data);}
+	print $socket "$data";
+	$socket->recv($data,2048);
+	if(!length($data) || !$socket->connected()) {
+	    print "No response from ncsvc, closing connection\n";
+	    INT_handler();
+	}
+	if($debug) {hdump($data);}
+	my $now = time - $start_t;
+	# printing RX/TX. This packet also contains encription type,
+	# compression and transport info, but length seems to be variable
+	printf("Duration: %02d:%02d:%02d  Sent: %s\tReceived: %s", 
+		int($now / 3600), int(($now % 3600) / 60), int($now % 60),
+		format_bytes(unpack('x[78]N',$data)), format_bytes(unpack('x[68]N',$data)));
+	sleep(1);
+}
+
+print "Exiting... Connect failed?\n";
+
+$socket->close();
+
+# for debugging
+sub hdump {
+	my $offset = 0;
+	my(@array,$format);
+	foreach my $data (unpack("a16"x(length($_[0])/16)."a*",$_[0])) {
+		my($len)=length($data);
+		if ($len == 16) {
+			@array = unpack('N4', $data);
+			$format="0x%08x (%05d)   %08x %08x %08x %08x   %s\n";
+		} else {
+			@array = unpack('C*', $data);
+			$_ = sprintf "%2.2x", $_ for @array;
+			push(@array, '  ') while $len++ < 16;
+			$format="0x%08x (%05d)" .
+				"   %s%s%s%s %s%s%s%s %s%s%s%s %s%s%s%s   %s\n";
+			
+		} 
+		$data =~ tr/\0-\37\177-\377/./;
+		printf $format,$offset,$offset,@array,$data;
+		$offset += 16;
+	}
+}
+
+# handle ctrl+c to logout and kill ncsvc 
+sub INT_handler {
+	# de-register handlers
+	$SIG{'INT'} = 'DEFAULT';
+	$SIG{'TERM'} = 'DEFAULT';
+	$SIG{'HUP'} = 'DEFAULT';
+	# re-enabling cursor
+	print "\e[?25h";
+	if($> == 0 && $dnsprotect) {
+		system("chattr -i /etc/resolv.conf");
+	}
+	if($socket->connected()){
+	    print "\nSending disconnect packet\n";
+    	    # disconnect packet
+	    $data="\0\0\0\0\0\0\0\x67\x01\0\0\0\x01\0\0\0\0\0\0\0";
+	    if($debug) {hdump($data);}
+	    print $socket "$data";
+	    $socket->recv($data,2048);
+	    print "Got reply\n";
+	    # xxx - we are ignoring reply
+	    if($debug) {hdump($data);}
+	}
+	print "Logging out...\n";
+	# do logout
+	$ua -> get ("https://$dhost:$dport/dana-na/auth/logout.cgi");
+	print "Killing ncsvc...\n";
+	# it is suid, so best is to use own api
+	system("./ncsvc -K");
+	# checking if resolv.conf correctly restored
+	if(-f "/etc/jnpr-nc-resolv.conf"){
+	    print "restoring resolv.conf\n";
+	    move("/etc/jnpr-nc-resolv.conf","/etc/resolv.conf");
+	}
+	print "Exiting\n";
+	exit(0);
+}
+
+sub parse_config_file {
+	
+	my $Name,my $Value; my $Config; my $File;
+	
+	($File, $Config) = @_;
+	
+	if (!open (CONFIG, "$File")) {
+		print "ERROR: Config file not found : $File\n";
+		exit(1);
+	}
+	
+	while (<CONFIG>) {
+		my $config_line=$_;
+		chop ($config_line);          # Get rid of the trailling \n
+		
+		$config_line =~ s/^\s*//;     # Remove spaces at the start of the line
+		$config_line =~ s/\s*$//;     # Remove spaces at the end of the line
+		if ( ($config_line !~ /^#/) && ($config_line ne "") ){    # Ignore lines starting with # and blank lines
+			($Name, $Value) = split (/=/, $config_line);          # Split each line into name value pairs
+			$$Config{$Name} = $Value;                             # Create a hash of the name value pairs
+		}
+	}
+	
+	close(CONFIG);
+	
+}
+
+sub read_password {
+	my $password = "";
+	my $pkey="";
+	# Start reading the keys
+	ReadMode(4); #Disable the control keys
+	while(ord($pkey = ReadKey(0)) != 10)
+	# This will continue until the Enter key is pressed (decimal value of 10)
+	{
+		# For all value of ord($key) see http://www.asciitable.com/
+		if(ord($pkey) == 127 || ord($pkey) == 8) {
+			# DEL/Backspace was pressed
+			#1. Remove the last char from the password
+			
+			#2 move the cursor back by one, print a blank character, move the cursor back by one
+			if (length($password)) {
+				print "\b \b";
+			}
+			chop($password);
+		} elsif(ord($pkey) < 32) {
+			# Do nothing with these control characters
+		} else {
+			$password = $password.$pkey;
+			print "*";
+		}
+	}
+	ReadMode(0); #Reset the terminal once we are done
+	return $password;
+}
+
+sub print_help {
+	print "Usage: $0 [--config <filename>] [-h]\n".
+		"\t-c, --config         configuration file, default jvpn.ini\n".
+		"\t-h, --help           print this text\n".
+		"Report jvpn bugs to samm\@os2.kiev.ua\n";
+	exit 0;
+}
+
+# i don`t want CPAN hell
+sub format_bytes
+{
+	my ($size) = @_;
+	
+	if ($size > 1099511627776)  #   TiB: 1024 GiB
+	{
+		return sprintf("%.2f TiB", $size / 1099511627776);
+	}
+	elsif ($size > 1073741824)  #   GiB: 1024 MiB
+	{
+		return sprintf("%.2f GiB", $size / 1073741824);
+	}
+	elsif ($size > 1048576)     #   MiB: 1024 KiB
+	{
+		return sprintf("%.2f MiB", $size / 1048576);
+	}
+	elsif ($size > 1024)        #   KiB: 1024 B
+	{
+		return sprintf("%.2f KiB", $size / 1024);
+	}
+	else                        #   B
+	{
+		return sprintf("%.2f B", $size);
+	}
+}
